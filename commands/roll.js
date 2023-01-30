18fb021bb9a61aaf28068fea69204ff02e2513be
Ryan Dawson <rdawson40601@gmail.com>
2023-01-30 16:02:10 -0500

    initial commit

---
+++ commands/roll.js
@@ -0,0 +1,27 @@
+/* eslint-disable indent */
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Roll a random number')
+        .addIntegerOption(option =>
+            option.setName('lower')
+                .setDescription('The lower bound of your random roll')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('upper')
+                .setDescription('The lower bound of your random roll')
+                .setRequired(true)),
+	async execute(interaction) {
+        const lower = interaction.options.getInteger('lower');
+        const upper = interaction.options.getInteger('upper');
+        const roll = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+
+        await interaction.reply(`Lower bound: ${lower} Upper bound: ${upper} Random roll result: ${roll}`);
+	},
+};
+
+// var upper =
+// var lower =
+// const roll = Math.floor(Math.random() * upper) + lower;
