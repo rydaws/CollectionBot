9be93a0442585e444ea6cee14beb37a0a0109901
Ryan Dawson <rdawson40601@gmail.com>
2023-02-15 18:44:15 -0500

    Updates start command to create DB entry for user

--- commands/start.js
+++ commands/start.js
@@ -2,31 +2,19 @@ const { SlashCommandBuilder } = require('discord.js');
 const { con } = require('../db');
 const { Client } = require('pg');
 
-let res;
-let name;
-let type;
-let level;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('start')
 		.setDescription('Creates profile for user'),
 	async execute(interaction) {
+		const id = interaction.user.id;
+		console.log(`Adding if not exists ${id}`);
 
 		const client = new Client(con);
 		await client.connect();
-		res = await client.query('SELECT * FROM monsters WHERE id=1');
+		await client.query(`INSERT INTO player VALUES (${id}, DEFAULT) ON CONFLICT (client_id) DO NOTHING`);
 
-		name = res.rows[0].display_name;
-		type = res.rows[0].type;
-		level = res.rows[0].level;
-		console.log(res);
-		console.log('Returning display name: ' + name);
-		console.log('Returning type: ' + type);
-		console.log('Returning level: ' + level);
-
-
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		await interaction.reply(`Created profile for ${interaction.user.username}`);
 		client.end();
 
 	},
